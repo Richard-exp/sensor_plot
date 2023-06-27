18076c5a2bf1b7ea9668c1314636eed76efeb529
Uhtomsky <r.aizpurietis@gmail.com>
2023-06-27 10:43:56 +0300

    simple_plot_thread_serial

--- src/main.rs
+++ src/main.rs
@@ -1,65 +1,47 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
-mod sensor_data;
-use sensor_data::SensorData;
+mod sensor_data_1;
+use sensor_data_1::SensorData;
 
+use egui::plot::{Line, Plot, PlotPoints};
 use eframe::egui;
-use egui::Vec2;
-use std::io::{self, Write};
-
-use std::collections::VecDeque;
-use std::sync::{Arc, Mutex};
-use std::thread;
 
 fn main() -> Result<(), eframe::Error> {
-    //let counter = Arc::new(Mutex::new(0));
-    let mut app = MyApp::new();
-    app.data.read_data();
-    //app.data.print_points(); - How to do this?
-
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
-    let options = eframe::NativeOptions {
-        initial_window_size: Some(egui::vec2(100.0, 100.0)),
-        always_on_top: false,
 
+    let options = eframe::NativeOptions {
+        initial_window_size: Some(egui::vec2(320.0, 240.0)),
         ..Default::default()
     };
-    eframe::run_native(
-        "My egui App",
-        options,
-        Box::new(|_| Box::new(app)),
-    )
-}
 
-pub struct MyApp {
-   data: SensorData,
-}
+    // Our application state:
+    let app_1 = SensorData::new();
+    app_1.read_data();
+    let app_2 = app_1;
+    let points = app_2.get_points();
 
-impl MyApp {
-    fn new() -> Self {
-        Self {
-            data: SensorData::new(),
-        }   
-    }    
-}
-
-impl eframe::App for MyApp {
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+    eframe::run_simple_native ("My egui App", options, move |ctx, _frame| {
         egui::CentralPanel::default().show(ctx, |ui| {
+            // let sin: PlotPoints = (0..1000).map(|i| {
+            //     let x = i as f64 * 0.01;
+            //     [x, x.sin()]
+            // }).collect();
+            // let line = Line::new(sin);
+            // Plot::new("my_plot").view_aspect(2.0).show(ui, |plot_ui| plot_ui.line(line));
             if ui.button("Exit").clicked() {
                 _frame.close();
             };
             
-            let mut plot = egui::plot::Plot::new("measurements").auto_bounds_y();
-
+            let plot = egui::plot::Plot::new("measurements").auto_bounds_y();
             plot.show(ui, |plot_ui| {
-                let points = self.data.get_points();
                 plot_ui.line(
                     egui::plot::Line::new(egui::plot::PlotPoints::Owned(Vec::from_iter(
                         points.iter().copied(),
                     ))), //self.measurements.lock().unwrap().plot_values(),
                 );
             });
+
         });
-    }
+    })
 }
+
