5b943b58fbafc72cef3e7d06441b5e938fe8cbb1
Uhtomsky <r.aizpurietis@gmail.com>
2023-07-02 18:39:56 +0300

    1st - thread::scope; 2nd - thread::spawn - join() at the end of main() - is not possible to borrow reference of struct; 3d - arc() - lost connection with fn update()

--- src/main.rs
+++ src/main.rs
@@ -12,12 +12,22 @@ use std::collections::VecDeque;
 // use std::thread;
 
 use serialport::SerialPort;
+use std::thread;
+use std::time::{Duration, Instant};
+use std::io::{self, Write};
+use std::sync::{Arc, Mutex};
 
-fn main() -> Result<(), eframe::Error> {
-
-    let mut app = MyApp::new_App();
-    sensor_data_1::SensorData::read_data(app);
-
+fn main() {
+    // let mut app = MyApp::new_app();
+    let app_2 = MyApp::new_app();
+    let app = Arc::new(Mutex::new(MyApp::new_app()));
+    let thread_1 = thread::spawn({
+    let app = Arc::clone(&app);
+    move || {
+    let mut app = app.lock().unwrap();
+    app.read_data();
+    }
+    });
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(100.0, 100.0)),
@@ -28,8 +38,9 @@ fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "My egui App",
         options,
-        Box::new(|_| Box::new(app)),
-    )
+        Box::new(|_| Box::new(app_2)),
+    ).expect("error in UI");
+    thread_1.join().expect("error in handling thread_1");
 }
 
 pub struct MyApp {
@@ -38,12 +49,39 @@ pub struct MyApp {
 }
 
 impl MyApp {
-    fn new_App() -> Self {
+    fn new_app() -> Self {
         Self {
             values: VecDeque::new(),
-            port: sensor_data_1::new_port(),
+            port: serialport::new("COM3", 9600)
+            .timeout(Duration::from_millis(1000))
+            .open()
+            .expect("Unable to connect COM port"),
         }   
-    }    
+    } 
+
+    fn read_data (&mut self) {
+                let mut serial_buf: Vec<u8> = vec![0; 100];
+                let mut buf_value: f64;
+            
+                let start_time = Instant::now();
+                loop {
+                    match self.port.read(serial_buf.as_mut_slice()) {
+                        Ok(t) => {
+                            let time_elapsed = Instant::now() - start_time;                   
+                            //io::stdout().write_all(&serial_buf[..t]).unwrap();
+                            buf_value = std::str::from_utf8(&serial_buf[..t]).unwrap().parse().unwrap();
+                            
+                            self.values.push_back(egui::plot::PlotPoint::new(time_elapsed.as_secs_f64(), buf_value));
+                        
+                        // println!("{buf_value}");
+                            //x += 20.0;
+                        // Point_vector.push(egui::plot::PlotPoint::new(x, buf_value));
+                        }
+                        Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+                        Err(e) => eprintln!("{:?}", e),
+                    } 
+                }
+    }
 }
 
 impl eframe::App for MyApp {
@@ -59,7 +97,7 @@ impl eframe::App for MyApp {
                 //let points = self.data.get_points();
                 plot_ui.line(
                     egui::plot::Line::new(egui::plot::PlotPoints::Owned(Vec::from_iter(
-                        self.values.iter().copied(),
+                        self.lock().unwrap().iter().copied(),
                     ))), //self.measurements.lock().unwrap().plot_values(),
                 );
             });
--- src/sensor_data_1.rs
+++ src/sensor_data_1.rs
@@ -1,50 +1,50 @@
-use eframe::egui;
-use serialport::SerialPort;
-use std::collections::VecDeque;
-use std::thread;
-use std::time::{Duration, Instant};
+// use eframe::egui;
+// use serialport::SerialPort;
+// use std::collections::VecDeque;
+// use std::thread;
+// use std::time::{Duration, Instant};
 
-use crate::MyApp;
+// use crate::MyApp;
 // use std::io::{self, Write};
-pub struct SensorData {
-    pub values: VecDeque<egui::plot::PlotPoint>,
-    port: Box<dyn SerialPort>,
-    another: MyApp,
-}
+// pub struct SensorData {
+//     pub values: VecDeque<egui::plot::PlotPoint>,
+//     port: Box<dyn SerialPort>,
+//     another: MyApp,
+// }
 
+    // pub fn new_port() -> Box<dyn SerialPort> {
+    //         serialport::new("COM3", 9600)
+    //             .timeout(Duration::from_millis(1000))
+    //             .open()
+    //             .expect("Unable to connect COM port")
+    // }
 
-    pub fn new_port() -> Box<dyn SerialPort> {
-            serialport::new("COM3", 9600)
-                .timeout(Duration::from_millis(1000))
-                .open()
-                .expect("Unable to connect COM port")
-    }
-  impl SensorData {
-    pub fn read_data (self) {
-        thread::spawn(|| {
-            let mut serial_buf: Vec<u8> = vec![0; 100];
-            let mut buf_value: f64;
+  //impl SensorData {
+    // pub fn read_data (self) {
+    //     thread::spawn(|| {
+    //         let mut serial_buf: Vec<u8> = vec![0; 100];
+    //         let mut buf_value: f64;
            
-            let start_time = Instant::now();
-            loop {
-                match self.port.read(serial_buf.as_mut_slice()) {
-                    Ok(t) => {
-                        let time_elapsed = Instant::now() - start_time;                   
-                        //io::stdout().write_all(&serial_buf[..t]).unwrap();
-                        buf_value = std::str::from_utf8(&serial_buf[..t]).unwrap().parse().unwrap();
+    //         let start_time = Instant::now();
+    //         loop {
+    //             match self.port.read(serial_buf.as_mut_slice()) {
+    //                 Ok(t) => {
+    //                     let time_elapsed = Instant::now() - start_time;                   
+    //                     //io::stdout().write_all(&serial_buf[..t]).unwrap();
+    //                     buf_value = std::str::from_utf8(&serial_buf[..t]).unwrap().parse().unwrap();
                         
-                        self.values.push_back(egui::plot::PlotPoint::new(time_elapsed.as_secs_f64(), buf_value));
+    //                     self.values.push_back(egui::plot::PlotPoint::new(time_elapsed.as_secs_f64(), buf_value));
                     
-                       // println!("{buf_value}");
-                        //x += 20.0;
-                       // Point_vector.push(egui::plot::PlotPoint::new(x, buf_value));
-                    }
-                    Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
-                    Err(e) => eprintln!("{:?}", e),
-                } 
-            }
-        });
-    }
+    //                    // println!("{buf_value}");
+    //                     //x += 20.0;
+    //                    // Point_vector.push(egui::plot::PlotPoint::new(x, buf_value));
+    //                 }
+    //                 Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+    //                 Err(e) => eprintln!("{:?}", e),
+    //             } 
+    //         }
+    //     });
+    // }
     
     // pub fn get_points() -> VecDeque<egui::plot::PlotPoint>{
     //     return self.values;
@@ -53,4 +53,4 @@ pub struct SensorData {
     // pub fn print_points() {
     //     println!("{:?}", self.values);
     // }
-}
+// }
